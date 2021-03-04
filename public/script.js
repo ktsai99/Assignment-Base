402fb4ac346414a1defe14d552d0af377d84b777
mhough1 <mhough113@gmail.com>
2021-03-04 17:49:23 -0600

    update

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const endpoint = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
+
+const cities = [];
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => cities.push(...data))
