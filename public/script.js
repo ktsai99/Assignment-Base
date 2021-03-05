2561b4010fb5f8637ac50b931d3c1aa1bef26b96
mhough1 <mhough113@gmail.com>
2021-03-04 18:31:40 -0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -8,3 +8,22 @@ fetch(endpoint,{method: 'post'})
     .then(blob => blob.json())
     .then(data => cities.push(...data))
     .then(console.log(cities));
+
+
+function findMatches(wordToMatch, cities)
+{
+    return cities.filter(place => 
+    {
+        const regex = new RegExp(wordToMatch,"gi");
+        return place.zip.match(regex) || place.name.match(regex) || place.type.match(regex);
+    });
+}
+
+function displayMatches()
+{
+    //const match
+}
+
+const searchInput = document.querySelector(".search");
+searchInput.addEventListener("change",displayMatches);
+searchInput.addEventListener("keyup",displayMatches);
